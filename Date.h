4c0dbf5859e6f966582dabca9a5874791ae85f96
Northfishall <370136921@qq.com>
2016-07-09 11:21:55 +0800

    Update and rename LICENSE.md to Date.h

---
+++ Date.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef Date_Homework
+#define Date_Homewoer
+class Date
+{
+public:
+	void setDate();
+	void showDate();
+	int getYear();
+	int getMonth();
+	int getDay();
+private:
+	int year;
+	int month;
+	int day;
+};
+
+#endif // !Date_Homework
+
